1794abbd8fd533182c14686732fcba491774d794
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2020-05-25 12:41:38 +0200

    fix pca run

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from data import get_mnist
-from sampling import split_grid
-from models import score_splits
-import numpy
 import sys
+
+import numpy
+
+from data import get_mnist
+from models import score_splits
+from sampling import split_grid
+
 
 def run(method, n_components, noise=None, seeds=20):
     x, y = get_mnist(method=method, n_components=n_components, noise=noise)
@@ -16,17 +19,17 @@ def run(method, n_components, noise=None, seeds=20):
 
 TODO = []
 
-for sigma in numpy.arange(0.05, 1.05, 0.05):
-    TODO.append(['pca', 8, numpy.sqrt(sigma)])
-
-for sigma in [0, 0.5, 1, 2, 3, 5]:
-    TODO.append(['pca', 8, sigma])
-    TODO.append(['rp', 32, sigma])
-    TODO.append(['rp', 128, sigma])
+# for sigma in numpy.arange(0.05, 1.05, 0.05):
+#     TODO.append(['pca', 8, numpy.sqrt(sigma)])
+#
+# for sigma in [0, 0.5, 1, 2, 3, 5]:
+#     TODO.append(['pca', 8, sigma])
+#     TODO.append(['rp', 32, sigma])
+#     TODO.append(['rp', 128, sigma])
 
 for n_components in range(2, 10):
-    TODO.append(['pca', n_components])
-    TODO.append(['rp', n_components])
+    TODO.append(['pca', 2**n_components])
+    TODO.append(['rp', 2**n_components])
 
 n = sys.argv[1]
 args = TODO[int(n)]
