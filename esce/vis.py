5efb8455ae8048244dc6e59c1c88bbd6188eb7cb
Alexander Koch <kochalexander@gmx.net>
2020-10-19 18:34:47 +0200

    Fix visualizer

--- esce/vis.py
+++ esce/vis.py
@@ -1,17 +1,17 @@
-import seaborn
+import seaborn as sns
 from matplotlib import pylab
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 
 def hp_plot(df):
-    df['log2_C'] = numpy.log2(df['C'])
-    df['log2_gamma'] = numpy.log2(df['gamma'])
+    df['log2_C'] = np.log2(df['C'])
+    df['log2_gamma'] = np.log2(df['gamma'])
 
     fig, ax = pylab.subplots(1, 3, dpi=200, sharey='row')
 
     ax_ = ax[0]
     df_ = df[(df.model == 'linear')]
-    seaborn.lineplot(x='log2_C', y='score_val', hue='n', data=df_, ax=ax_,
+    sns.lineplot(x='log2_C', y='score_val', hue='n', data=df_, ax=ax_,
                      legend=False, ci='sd')
     ax_.set_ylabel(f'score_val')
     ax_.set_xlabel('linear, log2_C')
@@ -19,25 +19,23 @@ def hp_plot(df):
     ax_ = ax[1]
     df_ = df[(df.model == 'rbf')]
     df_ = df_.groupby(['log2_C', 's','n'])['score_val'].max().reset_index()
-    seaborn.lineplot(x='log2_C', y='score_val', hue='n', data=df_, ax=ax_,
+    sns.lineplot(x='log2_C', y='score_val', hue='n', data=df_, ax=ax_,
                      legend=False, ci='sd')
     ax_.set_xlabel('rbf, log2_C')
 
     ax_ = ax[2]
     df_ = df[(df.model == 'rbf')]
     df_ = df_.groupby(['log2_gamma', 's','n'])['score_val'].max().reset_index()
-    seaborn.lineplot(x='log2_gamma', y='score_val', hue='n', data=df_, ax=ax_,
+    sns.lineplot(x='log2_gamma', y='score_val', hue='n', data=df_, ax=ax_,
                      legend='full', ci='sd')
     ax_.set_xlabel('rbf, log2_gamma')
 
     pylab.plt.show()
 
-
-
 def sc_plot(df, ax=False):
     idx = df.groupby(['n', 's', 'model'])['score_val'].idxmax()
     df_ = df.loc[idx]
-    seaborn.lineplot(x='n', y='score_test', hue='model', data=df_, ci='sd', ax=ax)
+    sns.lineplot(x='n', y='score_test', hue='model', data=df_, ci='sd')
     pylab.plt.xscale('log')
     pylab.plt.show()
 
