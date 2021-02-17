Fix random state and data standard scaling

esce/command_line.py
@@ -160,7 +160,9 @@ def datagen(
     x = StandardScaler().fit_transform(x)
 
     if noise > 0:
-        x = (x + noise * np.random.randn(*x.shape)) / np.sqrt(1 + noise ** 2)
+        np.random.seed(0)
+        x = (x + noise * np.random.randn(*x.shape))# / np.sqrt(1 + noise ** 2)
+        x = StandardScaler().fit_transform(x)
 
     # Generate default label and label noise
     labels = {"default": y}