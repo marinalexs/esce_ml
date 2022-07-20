459342ef904ed019e4d660294c36b0fbec568a89
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2022-07-20 16:24:41 +0200

    formatting

--- workflow/scripts/extrapolate.py
+++ workflow/scripts/extrapolate.py
@@ -6,6 +6,7 @@ import scipy.optimize
 from sklearn.metrics import r2_score
 import json
 
+
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -19,59 +20,65 @@ class NpEncoder(json.JSONEncoder):
 
 def extrapolate(
         stats_path: str,
-        extra_path:str,
-        bootstrap_path:str,
+        extra_path: str,
+        bootstrap_path: str,
         repeats: int) -> None:
     df = pd.read_csv(stats_path, index_col=False)
     metric = 'r2_val' if 'r2_val' in df.columns else 'acc_val'
-    result = {'n_seeds':len(df['s'].unique())}
+    result = {'n_seeds': len(df['s'].unique())}
 
-    x,y_mean, y_std, y_sem=[],[],[],[]
+    x, y_mean, y_std, y_sem = [], [], [], []
     for n in sorted(df['n'].unique()):
         x.append(n)
-        y_mean.append(df[df['n']==n][metric].mean())
-        y_std.append(df[df['n']==n][metric].std())
-        y_sem.append(df[df['n']==n][metric].std() / np.sqrt(result['n_seeds']))
-    p_mean, _ = scipy.optimize.curve_fit(lambda t,a,b,c: a*t**(-b)+c,  x, y_mean, sigma=None, maxfev=5000, p0=(-1,0.01,0.7),bounds=((-1, 0,0),(0,1,1)))
-   
+        y_mean.append(df[df['n'] == n][metric].mean())
+        y_std.append(df[df['n'] == n][metric].std())
+        y_sem.append(df[df['n'] == n][metric].std() /
+                     np.sqrt(result['n_seeds']))
+    p_mean, _ = scipy.optimize.curve_fit(lambda t, a, b, c: a*t**(-b)+c,  x, y_mean,
+                                         sigma=None, maxfev=5000, p0=(-1, 0.01, 0.7), bounds=((-1, 0, 0), (0, 1, 1)))
+
     p_bootstrap = []
     y_bootstrap = [[] for _ in x]
     for _ in range(repeats):
-        y=[]
-        for i,n in enumerate(x):
-            y_sample = df[df['n']==n][metric].sample(frac=1, replace=True).mean()
+        y = []
+        for i, n in enumerate(x):
+            y_sample = df[df['n'] == n][metric].sample(
+                frac=1, replace=True).mean()
             y.append(y_sample)
             y_bootstrap[i].append(y_sample)
         try:
-            p_, _ = scipy.optimize.curve_fit(lambda t,a,b,c: a*t**(-b)+c,  x, y, sigma=None, maxfev=5000, p0=(-1,0.01,0.7),bounds=((-1, 0,0),(0,1,1)))
+            p_, _ = scipy.optimize.curve_fit(lambda t, a, b, c: a*t**(-b)+c,  x, y,
+                                             sigma=None, maxfev=5000, p0=(-1, 0.01, 0.7), bounds=((-1, 0, 0), (0, 1, 1)))
             p_bootstrap.append(p_)
         except RuntimeError:
             print('failed to fit')
             continue
 
-    result['x']= x
-    result['y_mean']= y_mean
-    result['y_std']= y_std
-    result['y_sem']= y_sem
+    result['x'] = x
+    result['y_mean'] = y_mean
+    result['y_std'] = y_std
+    result['y_sem'] = y_sem
 
     x = np.asarray(x)
     y_mean = np.asarray(y_mean)
     y_sem = np.asarray(y_sem)
 
-    result['p_mean']= p_mean
+    result['p_mean'] = p_mean
     result['r2'] = r2_score(y_mean, p_mean[0]*x**(-p_mean[1])+p_mean[2])
-    result['chi2'] = sum((y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))**2/y_sem**2) / (len(x)-len(p_mean))
-    result['mu'],result['sigma'] = np.mean((y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))/y_sem), np.std((y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))/y_sem)
+    result['chi2'] = sum(
+        (y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))**2/y_sem**2) / (len(x)-len(p_mean))
+    result['mu'], result['sigma'] = np.mean(
+        (y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))/y_sem), np.std((y_mean-(p_mean[0]*x**(-p_mean[1])+p_mean[2]))/y_sem)
 
-    result['p_bootstrap_mean'] = np.mean(p_bootstrap ,0)
-    result['p_bootstrap_std'] = np.std(p_bootstrap ,0)
-    result['p_bootstrap_975'] = np.percentile(p_bootstrap ,97.5, axis=0)
-    result['p_bootstrap_025']= np.percentile(p_bootstrap ,2.5, axis=0)
+    result['p_bootstrap_mean'] = np.mean(p_bootstrap, 0)
+    result['p_bootstrap_std'] = np.std(p_bootstrap, 0)
+    result['p_bootstrap_975'] = np.percentile(p_bootstrap, 97.5, axis=0)
+    result['p_bootstrap_025'] = np.percentile(p_bootstrap, 2.5, axis=0)
 
-    result['y_bootstrap_mean'] = np.mean(y_bootstrap ,1)
-    result['y_bootstrap_std'] = np.std(y_bootstrap ,1)
-    result['y_bootstrap_975'] = np.percentile(y_bootstrap ,97.5, axis=1)
-    result['y_bootstrap_025']= np.percentile(y_bootstrap ,2.5, axis=1)
+    result['y_bootstrap_mean'] = np.mean(y_bootstrap, 1)
+    result['y_bootstrap_std'] = np.std(y_bootstrap, 1)
+    result['y_bootstrap_975'] = np.percentile(y_bootstrap, 97.5, axis=1)
+    result['y_bootstrap_025'] = np.percentile(y_bootstrap, 2.5, axis=1)
 
     with open(extra_path, 'w') as f:
         json.dump(result, f, cls=NpEncoder, indent=0)
@@ -79,4 +86,6 @@ def extrapolate(
     with open(bootstrap_path, 'w') as f:
         json.dump(p_bootstrap, f, cls=NpEncoder, indent=0)
 
-extrapolate(snakemake.input.scores, snakemake.output.stats,snakemake.output.bootstraps, snakemake.params.bootstrap_repetitions)
+
+extrapolate(snakemake.input.scores, snakemake.output.stats,
+            snakemake.output.bootstraps, snakemake.params.bootstrap_repetitions)
--- workflow/scripts/generate_splits.py
+++ workflow/scripts/generate_splits.py
@@ -3,11 +3,13 @@
 from typing import Dict, Iterable, List, Tuple, Optional, Union
 
 import numpy as np
-import yaml, os
+import yaml
+import os
 import pickle
 import json
 from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
+
 
 def generate_random_split(
     y: np.ndarray,
@@ -39,32 +41,34 @@ def generate_random_split(
         stratify=stratify,
         random_state=seed,
     )
-    split = {'idx_train': idx_originial[idx_train], 'idx_val': idx_originial[idx_val], 'idx_test': idx_originial[idx_test]}
+    split = {'idx_train': idx_originial[idx_train],
+             'idx_val': idx_originial[idx_val], 'idx_test': idx_originial[idx_test]}
     split = {k: v.tolist() for k, v in split.items()}  # for yaml
     split.update({'samplesize': n_train,
-                    'seed': seed,
-                    'stratify': do_stratify})  # metadata
+                  'seed': seed,
+                  'stratify': do_stratify})  # metadata
 
     return split
 
 
 def generate_matched_split(
-    y: np.ndarray,
-    match: np.ndarray,
-    n_train: int,
-    n_val: int = 1000,
-    n_test: int = 1000,
-    do_stratify: bool = False,
-    seed: int = 0,
-    mask: Optional[np.ndarray] = False,) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        y: np.ndarray,
+        match: np.ndarray,
+        n_train: int,
+        n_val: int = 1000,
+        n_test: int = 1000,
+        do_stratify: bool = False,
+        seed: int = 0,
+        mask: Optional[np.ndarray] = False,) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     random_state = np.random.RandomState(seed)
-    split = generate_random_split(y,n_train//2,n_val//2,n_test//2,do_stratify,seed, np.logical_and(mask, y>0))
+    split = generate_random_split(
+        y, n_train//2, n_val//2, n_test//2, do_stratify, seed, np.logical_and(mask, y > 0))
     idx_all = np.arange(len(y))
 
-    mask[split['idx_train']]=False
-    mask[split['idx_val']]=False
-    mask[split['idx_test']]=False
-    mask[y>0]=False
+    mask[split['idx_train']] = False
+    mask[split['idx_val']] = False
+    mask[split['idx_test']] = False
+    mask[y > 0] = False
 
     matching_scores = []
     for idx_set in ['idx_train', 'idx_val', 'idx_test']:
@@ -73,7 +77,7 @@ def generate_matched_split(
             idx_pool = idx_all[mask]
             scores = (match[mask]-match[idx])**2
 
-            t=random_state.permutation(np.column_stack((scores,idx_pool)))
+            t = random_state.permutation(np.column_stack((scores, idx_pool)))
             t_idx = np.argmin(t.T[0])
 
             score_match = t.T[0][t_idx]
@@ -83,15 +87,15 @@ def generate_matched_split(
             control_group.append(int(idx_match))
             mask[idx_match] = False
 
-        split[idx_set]+=control_group
+        split[idx_set] += control_group
 
-    split.update({'average_matching_score':np.mean(matching_scores)})
-    split['samplesize']*=2
+    split.update({'average_matching_score': np.mean(matching_scores)})
+    split['samplesize'] *= 2
 
     return split
-    
 
-def write_splitfile(features_path, targets_path, split_path, sampling_path,sampling_type, n_train, n_val, n_test, seed, stratify=False):
+
+def write_splitfile(features_path, targets_path, split_path, sampling_path, sampling_type, n_train, n_val, n_test, seed, stratify=False):
     x = np.genfromtxt(features_path, delimiter=',')
     x_mask = np.all(np.isfinite(x), 1)
 
@@ -103,32 +107,33 @@ def write_splitfile(features_path, targets_path, split_path, sampling_path,sampl
     n_classes = np.unique(y)
     idx_all = np.arange(len(y))
 
-    stratify = True if stratify and len(n_classes<=10) else False
+    stratify = True if stratify and len(n_classes <= 10) else False
 
     matching = np.genfromtxt(sampling_path, delimiter=',')
-    if sampling_type =='raw':
+    if sampling_type == 'raw':
         matching = False
-    elif sampling_type =='balanced':
+    elif sampling_type == 'balanced':
         matching = False
-        idx_undersampled = RandomUnderSampler(random_state=seed).fit_resample(idx_all[xy_mask],y[xy_mask])
+        idx_undersampled = RandomUnderSampler(
+            random_state=seed).fit_resample(idx_all[xy_mask], y[xy_mask])
         mask[[i for i in idx_all if i not in idx_undersampled]] = False
-    elif len(matching)==len(y) and len(matching.shape)>1:
+    elif len(matching) == len(y) and len(matching.shape) > 1:
         assert n_classes == 2
         m_mask = np.all(np.isfinite(matching), 1)
-        xy_mask = np.logical_and(xy_mask,m_mask)
-    elif len(matching)==len(y) and len(matching.shape)==1:
+        xy_mask = np.logical_and(xy_mask, m_mask)
+    elif len(matching) == len(y) and len(matching.shape) == 1:
         assert n_classes == 2
         m_mask = np.isfinite(matching)
-        xy_mask = np.logical_and(xy_mask,m_mask)
+        xy_mask = np.logical_and(xy_mask, m_mask)
     else:
         raise Exception('invalid sampling file')
 
-    if  matching is False and sum(xy_mask) > n_train + n_val + n_test:
+    if matching is False and sum(xy_mask) > n_train + n_val + n_test:
         split_dict = generate_random_split(y=y, n_train=n_train, n_val=n_val,
-                                    n_test=n_test, do_stratify=stratify, mask=xy_mask, seed=seed)
-    elif sum(xy_mask[y==1]) > n_train//2 + n_val//2 + n_test//2:
+                                           n_test=n_test, do_stratify=stratify, mask=xy_mask, seed=seed)
+    elif sum(xy_mask[y == 1]) > n_train//2 + n_val//2 + n_test//2:
         split_dict = generate_matched_split(y=y, match=matching, n_train=n_train, n_val=n_val,
-                                    n_test=n_test, do_stratify=stratify, mask=xy_mask, seed=seed)
+                                            n_test=n_test, do_stratify=stratify, mask=xy_mask, seed=seed)
     else:
         split_dict = {'error': 'insufficient samples'}
 
@@ -137,8 +142,10 @@ def write_splitfile(features_path, targets_path, split_path, sampling_path,sampl
 
 
 n_train = int(snakemake.wildcards.samplesize)
-n_val = min(round(n_train*snakemake.params.val_test_frac),snakemake.params.val_test_max)
-n_test = min(round(n_train*snakemake.params.val_test_frac),snakemake.params.val_test_max)
+n_val = min(round(n_train*snakemake.params.val_test_frac),
+            snakemake.params.val_test_max)
+n_test = min(round(n_train*snakemake.params.val_test_frac),
+             snakemake.params.val_test_max)
 
-write_splitfile(features_path=snakemake.input.features, targets_path=snakemake.input.targets, split_path=snakemake.output.split, sampling_path=snakemake.input.sampling,sampling_type=snakemake.wildcards.sampling, n_train=n_train,
+write_splitfile(features_path=snakemake.input.features, targets_path=snakemake.input.targets, split_path=snakemake.output.split, sampling_path=snakemake.input.sampling, sampling_type=snakemake.wildcards.sampling, n_train=n_train,
                 n_val=n_val, n_test=n_test, seed=int(snakemake.wildcards.seed), stratify=snakemake.params.stratify)
