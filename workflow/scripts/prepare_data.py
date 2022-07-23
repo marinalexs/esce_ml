5f444e25f45e10ca807b87e1f60c2f2ff1153426
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2022-07-23 19:41:50 +0200

    custom dataset definition (

---
+++ workflow/scripts/prepare_data.py
@@ -0,0 +1,49 @@
+from sklearn.datasets import fetch_openml
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from pathlib import Path
+import pandas as pd
+
+
+def download_mnist(
+    out_path: str,
+    dataset: str,
+    features_targets_covariates: str,
+    variant: str,
+    custom_datasets: dict,
+) -> None:
+    if dataset == 'mnist' and variant == 'pixel':
+        x, _ = fetch_openml("mnist_784", version=1,
+                            return_X_y=True, as_frame=False)
+        data = x
+    elif dataset == 'mnist' and variant == 'ten-digits':
+        _, y = fetch_openml("mnist_784", version=1,
+                            return_X_y=True, as_frame=False)
+        data = y.astype(int)
+    elif dataset == 'mnist' and variant == 'odd-even':
+        _, y = fetch_openml("mnist_784", version=1,
+                            return_X_y=True, as_frame=False)
+        data = (y.astype(int) % 2 == 0).astype(int)
+    elif features_targets_covariates == 'covariates' and variant in ['none', 'balanced']:
+        data = []
+    else:
+        in_path = Path(custom_datasets[dataset][features_or_targets][name])
+        if path.suffix == '.csv':
+            data = pd.read_csv(in_path)
+        if path.suffix == '.tsv':
+            data = pd.read_csv(in_path, delimiter='\t')
+        if path.suffix == '.npy':
+            data = np.load(in_path)
+    if features_targets_covariates == 'features':
+        data = StandardScaler().fit_transform(data)
+    np.save(out_path, data)
+
+
+download_mnist(
+    snakemake.output.npy,
+    snakemake.wildcards.dataset,
+    snakemake.wildcards.features_targets_covariates,
+    snakemake.wildcards.name,
+    snakemake.params.custom_datasets,
+)
