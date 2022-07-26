5c140a7ec97e100fb711fc91e4f3a43894b35a9f
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2022-07-26 23:00:01 +0200

    nanmask for confound regression

--- workflow/scripts/confound_regression.py
+++ workflow/scripts/confound_regression.py
@@ -1,23 +1,23 @@
-import pandas as pd
-import os
-from pathlib import Path
 import numpy as np
-import scipy.optimize
 from sklearn.linear_model import LinearRegression
-import json
 
 
-def confound_regression(data_path, confounds_path, out_path):
+def confound_regression(data_path: str, confounds_path: str, out_path: str):
     data_raw = np.load(data_path)
     confounds = np.load(confounds_path)
+
     if len(data_raw.shape) == 1:
         data_raw = data_raw.reshape(-1, 1)
     if len(confounds.shape) == 1:
         confounds = confounds.reshape(-1, 1)
     assert len(data_raw) == len(confounds)
 
+    x_mask = np.all(np.isfinite(data_raw), 1)
+    y_mask = np.all(np.isfinite(confounds), 1)
+    xy_mask = np.logical_and(x_mask, y_mask)
+
     model = LinearRegression()
-    model.fit(confounds, data_raw)
+    model.fit(confounds[xy_mask], data_raw[xy_mask])
     data_predicted = model.predict(confounds)
     data_corrected = data_raw - data_predicted
 
@@ -25,5 +25,5 @@ def confound_regression(data_path, confounds_path, out_path):
 
 
 confound_regression(
-    snakemake.input.features, snakemake.input.sampling, snakemake.output.features
+    snakemake.input.features, snakemake.input.confounds, snakemake.output.features
 )
