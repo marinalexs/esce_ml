7cd8d9cba89b6553b4ca94834198b3452b6686e6
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2023-06-27 17:56:48 +0200

    add mean regressor model

--- workflow/scripts/models.py
+++ workflow/scripts/models.py
@@ -3,13 +3,16 @@ import sys
 sys.path.append(str(Path(__file__).parent))
 
 from base_models import ClassifierModel, RegressionModel
-from sklearn.dummy import DummyClassifier
+from sklearn.dummy import DummyClassifier, DummyRegressor
 from sklearn.linear_model import Ridge, RidgeClassifier
 
 MODELS = {
     "majority-classifier": ClassifierModel(
         lambda **args: DummyClassifier(strategy="most_frequent", **args),
         "majority classifier",
+    ),
+    "mean-regressor": RegressionModel(
+        lambda **args: DummyRegressor(strategy="mean", **args), "mean regressor"
     ),
     "ridge-cls": ClassifierModel(
         lambda **args: RidgeClassifier(**args), "ridge classifier"
